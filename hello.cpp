25cef2502ec818d9fc916f20a82c3cfbebf7067a
HackX-OP <utkarshraj8800153091@gmail.com>
2020-11-21 23:00:34 +0530

    swap

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{   int a,b;
+     cout<<"Enter numbers to swap hahaha"<<endl;
+     cin>>a>>b;
+     cout<<"Numbers brfore swapping"<<endl;
+     cout<<"a="<<a<<" "<<"b="<<b<<endl;
+     int t;
+     t=a;
+     a=b;
+     b=t;
+     cout<<"Numbers after swapping"<<endl;
+     cout<<"a="<<a<<" "<<"b="<<b<<endl;
+    return 0;
+
+}
